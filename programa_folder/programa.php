6a3807c465e1758c4975725250da00deccd34612
francescroycampderros <francescroy@francescroy.com>
2024-10-01 16:24:46 +0200

    using a function from another file

--- programa_folder/programa.php
+++ programa_folder/programa.php
@@ -1,8 +1,11 @@
 
 <?php
 
-require __DIR__ . '/vendor/autoload.php';
+require __DIR__ . '/../vendor/autoload.php';
+require __DIR__ . '/programa2.php';
 
 $log = new Monolog\Logger('name');
 $log->pushHandler(new Monolog\Handler\StreamHandler('app.log', Monolog\Logger::WARNING));
 $log->warning('Foo');
+
+echo first(1, "omg lol"); //returns omg lol;
