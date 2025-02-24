7d855ba912bd47232b034f151d3e01c69b69f1c7
francescroycampderros <francescroy@francescroy.com>
2025-02-24 16:54:53 +0100

    Moving forward

--- programa_folder/programa.php
+++ programa_folder/programa.php
@@ -4,8 +4,14 @@
 require __DIR__ . '/../vendor/autoload.php';
 require __DIR__ . '/programa2.php';
 
-$log = new Monolog\Logger('name');
-$log->pushHandler(new Monolog\Handler\StreamHandler('app.log', Monolog\Logger::WARNING));
-$log->warning('Foo');
 
-echo first(1, "omg lol"); //returns omg lol;
+function myMessage() {
+
+	$log = new Monolog\Logger('name');
+	$log->pushHandler(new Monolog\Handler\StreamHandler('app.log', Monolog\Logger::WARNING));
+	$log->warning('Foo');
+
+	echo first(1, "omg lol"); //returns omg lol;
+}
+
+myMessage();
